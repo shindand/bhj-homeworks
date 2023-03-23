bed044734f95e9d9c17fdb42bc99e9152dcd54e0
warvic <warvic@yandex.ru>
2023-03-23 10:39:18 +0300

    исправлено

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -2,8 +2,10 @@
 const moleDead = document.getElementById("dead");
 const moleLost = document.getElementById("lost");
 
-const stopGame = () => {
-    return moleDead.textContent = moleLost.textContent = 0;
+const stopGame = (finish) => {
+    alert(finish);
+    moleDead.textContent = 0;
+    moleLost.textContent = 0;
 }
 
 for (let index = 1; index < 10; index++) {
@@ -14,15 +16,15 @@ for (let index = 1; index < 10; index++) {
   getHole(index).onclick = function () {
     if (getHole(index).className == "hole hole_has-mole") {
       moleDead.textContent++;
-      if (moleDead.textContent == 10) {
-        alert("Стоп игра. Вы победили!!! 😍");
-        stopGame();
+      if (moleDead.textContent == 1) {
+        let finish = "Стоп игра. Вы победили!!! 😍";
+        stopGame(finish);
       }
     } else {
       moleLost.textContent--;
       if (moleLost.textContent == -5) {
-        alert("Стоп игра. Вы продули!😡");
-        stopGame();
+        let finish = "Стоп игра. Вы продули!😡";
+        stopGame(finish);
       }
     }
   }
