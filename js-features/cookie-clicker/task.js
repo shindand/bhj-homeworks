97980d1cd29c8c071f3384b5f65923844da7d184
warvic <warvic@yandex.ru>
2023-03-23 10:41:25 +0300

    не получается

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,19 +2,26 @@ const image = document.getElementById("cookie");
 const clicker = document.getElementById("clicker__counter");
 const speed = document.getElementById("clicker__speed");
 
+let firstTime = new Date();
+
 function changeSizes() {   
-    image.width = image.width == "300" ? "200" : "300";
-    clicker.textContent++;
-    clicker.textContent = clicker.textContent > 39 ? "0" : clicker.textContent; 
-    
-    clickTime();
+
+    image.width = ++clicker.textContent % 2 ? 250 : 200;
+    clicker.textContent = clicker.textContent > 39 ? 0 : clicker.textContent; 
+  
+  
+    clickTime(firstTime);
 
 };
 
 image.onclick = changeSizes;
 
-function clickTime() {
+function clickTime(firstTime) {
     let newDate = new Date();
-    speed.textContent = ((1 / newDate.getMilliseconds()) * 1000).toFixed(2);
+    console.log(firstTime.getMilliseconds(), newDate.getMilliseconds());
+    speed.textContent = ((1 / (firstTime.getMilliseconds() - newDate.getMilliseconds())) * 1000).toFixed(2);
+    firstTime = new Date();
+    return firstTime;
+    
 }
 
