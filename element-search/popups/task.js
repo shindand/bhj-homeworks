71627d682c38551ff39b343cf04c9ca7c983adab
warvic <warvic@yandex.ru>
2023-03-23 15:46:54 +0300

    задание 1

---
+++ element-search/popups/task.js
@@ -0,0 +1,17 @@
+let modal = document.getElementById("modal_main");
+modal.className = "modal modal_active";
+
+let closeModal = document.getElementsByClassName("modal__close modal__close_times");
+
+let modalSmall = document.getElementById("modal_success");
+let linksModal = document.getElementsByClassName("show-success");
+
+
+ Array.from(linksModal).forEach(item => item.onclick = function () {
+    modalSmall.className = "modal modal_active";   
+});
+
+Array.from(closeModal).forEach(item => item.onclick = function () {
+    modalSmall.className = "modal";    
+    modal.className = "modal";   
+});
