8d0948a9ba2354e8be3f0b34bff20e408f7c4c38
warvic <warvic@yandex.ru>
2023-03-28 13:27:02 +0300

    исправлено

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,25 +1,28 @@
-const menuUp = document.getElementsByClassName("menu menu_sub");
-const menuLink = document.getElementsByClassName("menu__link");
-
-const menuLink2 = document.querySelectorAll(".menu__link");
-
+const menuLink = document.querySelectorAll(".menu__link");
 const queryMenu = document.querySelectorAll(".menu_sub");
 
 let oldIndex = 0;
+let oldPerent = 0;
 
-  Array.from(menuLink2).forEach((item, index) => item.onclick = function() { 
+  Array.from(menuLink).forEach((item, index) => item.onclick = function() { 
 
-  if (oldIndex != 0 && oldIndex != index) {
-     menuLink2[++oldIndex].closest(".menu_sub").className = "menu menu_sub";
-   }
+    let findPerent = index < menuLink.length - 1 ? index + 1: index;
 
-    menuLink2[++index].closest(".menu_sub").className.includes("menu_active") ? menuLink2[index + 1].closest(".menu_sub").className = "menu menu_sub" : 
-  menuLink2[++index].closest(".menu_sub").className = "menu menu_sub menu_active";
-  
-    oldIndex = index;
-  
-    return false;
+    count(oldPerent);
+    if (oldPerent != 0 && oldPerent != findPerent) {
+      menuLink[oldPerent].closest(".menu_sub").className = "menu menu_sub";
+      }
+      
+      oldPerent = findPerent;   
+
+    if (menuLink[findPerent].closest(".menu_sub") != null) {
+      menuLink[findPerent].closest(".menu_sub").className.includes("menu_active") ? menuLink[findPerent].closest(".menu_sub").className = "menu menu_sub" : 
+      menuLink[findPerent].closest(".menu_sub").className = "menu menu_sub menu_active";
+      return false;
+    }
+
   });
 
-
-  
+  function count(oldPerent) {
+    return oldPerent;
+  }
