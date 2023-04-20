c52605ee4b7cef82c3fd0dfc68d1b48398bd530e
warvic <warvic@yandex.ru>
2023-04-20 09:15:37 +0300

    interests

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -2,29 +2,31 @@ const interestCheck = document.querySelectorAll('.interest__check');
 const interestsActive = document.querySelectorAll('.interests.interests_active');
 
  Array.from(interestCheck).forEach((item, index) => {
-    
-   console.log(item, index);
 
     item.addEventListener('change', () => 
  {
-     Array.from(interestsActive).forEach(next => {
-       // while(item.nextSibling != 'ul')
-        console.log(item.nextSibling);
-     });
-    });
-   // console.log(item.nextSibling);
-    //console.log(item.nextSibling);
-     //console.log(item.closest('ul'));
 
+  if ((index + 1) < interestCheck.length && interestCheck[index + 1].closest('ul.interests.interests_active') && !item.closest('ul.interests.interests_active')) {
 
+  for (let i = index + 1; i < interestCheck.length; i++) {
+    if (interestCheck[i].closest('.interests.interests_active')) {
+      if (interestCheck[i].checked == true) {
+        interestCheck[i].checked = false;
+      }
+      else {
+        interestCheck[i].checked = true;
+      }
+    }
+    else {return;}
+  }
+  }
 
-    
+  });
 });
+
+
+
+
     
-    
-    
-    
-    
-    
-  //  item.checked = true);
+
     
