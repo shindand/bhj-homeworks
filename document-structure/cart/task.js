27f54314f1fc87419c4ae914c89056cf32622947
warvic <warvic@yandex.ru>
2023-04-29 17:16:54 +0300

    исправлено

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,6 +1,5 @@
 const product = document.querySelectorAll('.product');
 const productAdd = document.querySelectorAll('.product__add');
-//const productControls = document.querySelectorAll('.product__controls');
 const cartProducts = document.querySelector('.cart__products');
 const productImage = document.querySelectorAll('img.product__image');
 const plus = document.querySelectorAll('.product__quantity-control.product__quantity-control_inc');
@@ -8,20 +7,13 @@ const dec = document.querySelectorAll('.product__quantity-control.product__quant
 const productQuantity = document.querySelectorAll('.product__quantity-value');
 
 const products = document.querySelector('.products');
-let count = [];
-for (let i = 0; i < products.children.length; i ++) {
-  count[i] = 1;
-}
-
-let noDouble = [];
 
 Array.from(product).forEach((item, index) => 
 {
-
    plus[index].addEventListener('click', function() {
     productQuantity[index].innerHTML = `
       <div class="product__quantity-value">
-        ${count[index] += 1}
+      ${++productQuantity[index].textContent}
       </div>
    `;
 });
@@ -29,21 +21,29 @@ Array.from(product).forEach((item, index) =>
    dec[index].addEventListener('click', function() {
     productQuantity[index].innerHTML = `
       <div class="product__quantity-value">
-        ${count[index] = count[index] > 1 ? count[index] - 1 : 1}
+      ${productQuantity[index].textContent = productQuantity[index].textContent > 1 ? --productQuantity[index].textContent : 1}
       </div>
    `;
 });
 
    productAdd[index].addEventListener('click', function() {
-    if (noDouble[index] != true) {
-      noDouble[index] = true;
+
+     const card = document.querySelectorAll('.cart__product');
+     const productCount = document.querySelectorAll('.cart__product-count');
+
+     const prodId = Array.from(card).findIndex(item => item.dataset.id == product[index].dataset.id);
+
+ if (prodId == -1) {
       cartProducts.innerHTML += `
-        <div class="cart__product" data-id="${product[index].dataset.id}">
-          <img class="cart__product-image" src="${productImage[index].src}">
-          <div class="cart__product-count"> ${count[index]}</div>
-        </div>
-      `
-    }
-    });
-    
+          <div class="cart__product" data-id="${product[index].dataset.id}">
+            <img class="cart__product-image" src="${productImage[index].src}">
+            <div class="cart__product-count"> ${productQuantity[index].textContent}</div>
+         </div>
+       `
+    } 
+    else {
+     productCount[prodId].textContent = productQuantity[index].textContent; 
+     }
 });
+
+})
