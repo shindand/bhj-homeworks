7234164329b9fa64ab8570cc562a1601629327e4
warvic <warvic@yandex.ru>
2023-04-29 17:18:47 +0300

    исправлено

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -2,11 +2,7 @@ const hasTooltip = document.querySelectorAll('.has-tooltip');
 
 Array.from(hasTooltip).forEach((item, index) => {
     let {left, top} = item.getBoundingClientRect();
-    item.innerHTML += `
-      <div class="tooltip" style="left: ${left}; top: ${top}">
-        ${item.title}
-      </div>
-    `;
+    item.insertAdjacentHTML("afterEnd", `<div class='tooltip' style='left: ${left}; top: ${top}'>${item.title}</div>`);
   });
 
 const tooltip = document.querySelectorAll('.tooltip');
@@ -14,10 +10,17 @@ const tooltip = document.querySelectorAll('.tooltip');
 let i = null;
 
 Array.from(hasTooltip).forEach((item, index) => item.onclick = function() {
-    if (i != null) {
+    if (i != null && i != index) {
         tooltip[i].classList.remove('tooltip_active');
     }
-    tooltip[index].classList.add('tooltip_active');
-    i = index;
+    if (i == index) {
+      tooltip[index].classList.toggle('tooltip_active');
+      console.log(i, index);
+      console.log(tooltip[index].classList);
+    }
+    else {
+      tooltip[index].classList.add('tooltip_active');
+      i = index;
+    }
     return false;
   });
